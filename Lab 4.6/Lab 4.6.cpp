d677398d3e3d53a50b68c6363e28f81870de35e5
Volodya22211 <Volodymyr.Baran.RI.2023@lpnu.ua>
2023-10-14 17:16:35 +0300

    Add project files.

---
+++ Lab 4.6/Lab 4.6.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int i, k;
+	double S, P;
+
+	P = 1;
+	i = 1;
+	while (i <= 15) {
+		S = 0;
+		k = 1;
+		while (k <= i) {
+			S += 1. / k;
+			k++;
+		}
+		P *= (pow(sin(i), 2) + pow(cos(S), 2)) / pow(i, 2);
+		i++;
+	}
+	cout << "P: " << P << endl;
+	P = 1;
+	i = 1;
+	do {
+		S = 0;
+		k = 1;
+		do {
+			S += 1. / k;
+			k++;
+		} while (k <= i);
+		P *= (pow(sin(i), 2) + pow(cos(S), 2)) / pow(i, 2);
+		i++;
+	} while (i <= 15);
+	cout << "P: " << P << endl;
+
+	P = 1;
+	for (i = 1; i <= 15; i++) {
+		S = 0;
+		for (k = 1; k <= i; k++) {
+			S += 1. / k;
+		}
+		P *= (pow(sin(i), 2) + pow(cos(S), 2)) / pow(i, 2);
+	}
+	cout << "P: " << P << endl;
+
+	P = 1;
+	for (i = 15; i >= 1; i--) {
+		S = 0;;
+		for (k = i; k >= 1; k--) {
+			S += 1. / k;
+		}
+		P *= (pow(sin(i), 2) + pow(cos(S), 2)) / pow(i, 2);
+	}
+	cout << "P: " << P << endl;
+	return 0;
+}
